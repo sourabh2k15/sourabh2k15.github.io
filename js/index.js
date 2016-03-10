8df38d8565edc190cec519b2e09968b0a9934043
sourab reddy <sourab.reddy2k14@gmail.com>
2016-03-10 21:34:02 +0530

    added projects

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    console.log("jquery working!!");
+    document.getElementById('project_1').click();
+});
+
+function opendrawer(obj){
+    $('#'+obj.dataset.link).slideToggle(500);
+}
