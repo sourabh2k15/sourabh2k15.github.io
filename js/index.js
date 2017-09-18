36c0e4ceba814cc0bb384c8ab228a7124435eae9
sourabh2k15 <sourab.reddy2k14@gmail.com>
2017-09-18 04:09:54 -0400

    keeping project drawers open by default

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     console.log("jquery working!!");
+    document.getElementById('project_1').click();	
     document.getElementById('project_2').click();
+    document.getElementById('project_3').click();
+    document.getElementById('project_4').click();	
 });
 
 function opendrawer(obj){
